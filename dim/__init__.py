85a900c5d47dd8729b9d39152ebab4bda3c97ec5
ryo-ma <saka_ro@yahoo.co.jp>
2023-01-03 15:27:09 +0900

    Specify import function

--- dim/__init__.py
+++ dim/__init__.py
@@ -2,4 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from dim.__version__ import __version__  # noqa: F401
-from dim.dim import *  # noqa: F403, F401
+from dim.dim import (load_data, fetch_data,  # noqa: F401
+                     load_dim_json, load_dim_lock_json,
+                     init, install, uninstall,
+                     update, list, search)
